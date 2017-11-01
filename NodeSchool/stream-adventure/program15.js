ade840a77ed62a7918a1e27eb970ec37f987c544
youlingman <chenyoucyl@gmail.com>
2017-11-01 18:22:31 +0800

    some ci for nodeschool

--- NodeSchool/stream-adventure/program15.js
+++ NodeSchool/stream-adventure/program15.js
@@ -1,16 +1,23 @@
 var crypto = require('crypto');
 var zlib = require('zlib');
 var tar = require('tar');
+var through = require('through2');
 var parser = tar.Parse();
 
 parser.on('entry', function (entry) {
     // console.dir(e);
-    crypto.createHash('md5', { encoding: 'hex' });
-    entry.pipe();
+	if(entry.type === 'File') {
+		entry
+            .pipe(crypto.createHash('md5', { encoding: 'hex' }))
+            .pipe(through(function(data, _, next) {
+                this.push(data.toString() + ' ' + entry.path + '\n');
+                next();
+            }))
+            .pipe(process.stdout);
+	}
 });
 
 process.stdin
     .pipe(crypto.createDecipher(process.argv[2], process.argv[3]))
     .pipe(zlib.createGunzip())
     .pipe(parser);
-    // .pipe(process.stdout);
